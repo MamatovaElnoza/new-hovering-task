2abbdb5dc8f3eed5493fc111140a41df95acf2e9
MamatovaElnoza <mamatovaelnoza@gmail.com>
2024-11-15 14:19:13 +0300

    NOT finished Hovering effect

---
+++ script.js
@@ -0,0 +1,15 @@
+const words = document.querySelectorAll('.hover-word');
+
+words.forEach(word => {
+    word.addEventListener('mouseenter', (event) => {
+        const listId = event.target.getAttribute('data-list');
+        const list = document.getElementById(listId);
+        list.style.display = 'block';
+    });
+
+    word.addEventListener('mouseleave', (event) => {
+        const listId = event.target.getAttribute('data-list');
+        const list = document.getElementById(listId);
+        list.style.display = 'none';
+    });
+});
